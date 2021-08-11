63aec7c36cb97970f112fa28b978ab2211a1961d
marzan-666 <38738228+marzan-666@users.noreply.github.com>
2021-08-11 19:23:02 +0600

    :)

---
+++ Object Repository/Food/Breakfast/Page_Breakfast - Chaldal Online Grocery Sho_6c6dba/div_Jam  Spreads.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Jam  Spreads</name>
+   <tag></tag>
+   <elementGuidId>a1baabc4-e8ce-42c0-8891-da55f4398496</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='page']/div/div[5]/section/div/div/div/div/section/div[2]/div/a[4]/div/div[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>name</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-reactid</name>
+      <type>Main</type>
+      <value>.1u6cnknu226.6.2.0.0.0.0.2.4.0.$54.0.1</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Jam &amp; Spreads</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;page&quot;)/div[@class=&quot;app catalog navOpen chaldal-theme&quot;]/div[@class=&quot;everythingElseWrapper&quot;]/section[@class=&quot;bodyTable&quot;]/div[1]/div[@class=&quot;catalogBrowser&quot;]/div[@class=&quot;loaded&quot;]/div[1]/section[@class=&quot;bodyWrapper&quot;]/div[@class=&quot;category-blocks-wrapper&quot;]/div[@class=&quot;category-links-wrapper&quot;]/a[@class=&quot;category&quot;]/div[1]/div[@class=&quot;name&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='page']/div/div[5]/section/div/div/div/div/section/div[2]/div/a[4]/div/div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cereals'])[2]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Energy Boosters'])[2]/following::div[6]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[4]/div/div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
