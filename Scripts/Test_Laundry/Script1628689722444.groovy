2a249d34e23550fb3fb207266413353740c37d9b
marzan-666 <38738228+marzan-666@users.noreply.github.com>
2021-08-11 19:49:35 +0600

    26th Commit

---
+++ Scripts/Test_Laundry/Script1628689722444.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://chaldal.com/')
+
+WebUI.click(findTestObject('Object Repository/Home Cleaning/Laundry/Page_Chaldal  Online Grocery Shopping and D_1c8b72/a_Home  Cleaning'))
+
+WebUI.click(findTestObject('Object Repository/Home Cleaning/Laundry/Page_Home  Cleaning - Chaldal Online Grocer_2acf5a/span_Home  Cleaning'))
+
+WebUI.click(findTestObject('Object Repository/Home Cleaning/Laundry/Page_Home  Cleaning - Chaldal Online Grocer_2acf5a/div_Home  Cleaning'))
+
+WebUI.click(findTestObject('Object Repository/Home Cleaning/Laundry/Page_Home  Cleaning - Chaldal Online Grocer_2acf5a/div_Laundry'))
+
+WebUI.click(findTestObject('Object Repository/Home Cleaning/Laundry/Page_Laundry - Chaldal Online Grocery Shopp_88f265/div_Laundry'))
+
+WebUI.click(findTestObject('Object Repository/Home Cleaning/Laundry/Page_Laundry - Chaldal Online Grocery Shopp_88f265/li_Laundry'))
+
+WebUI.closeBrowser()
+
