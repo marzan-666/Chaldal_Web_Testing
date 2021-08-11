7911533ac12a9b7a16a62ec6ee61e13ff090aa30
marzan6666 <38738228+marzan6666@users.noreply.github.com>
2021-08-11 20:19:10 +0600

    Final Commit

---
+++ Test Suites/Chaldal_Test_Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Chaldal_Test_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cefa13c1-2a4f-4fee-878b-87281f80b8c0</testSuiteGuid>
+</TestSuiteEntity>
